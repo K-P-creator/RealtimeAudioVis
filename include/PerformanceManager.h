56b786d463abd02ab7baaf415e47289baf371070
Kenny <kpiccirilliplatt@gmail.com>
2026-02-12 20:08:04 -0800

    Added perf overlay api functions

--- include/PerformanceManager.h
+++ include/PerformanceManager.h
@@ -28,6 +28,15 @@ public:
     void writePerformanceData();    //  Writes a performance report to json in benchmarks/out
                                     //  Report metrics - average/min/max/p50/p95/p99 for each timer
 
+    //  Performance Overlay API functions
+    [[nodiscard]] us getCurrentFFTTime() const;
+    [[nodiscard]] us getCurrentRenderTime() const;
+    [[nodiscard]] us getCurrentFrameTime() const;
+
+    [[nodiscard]] us getAverageFFTTime() const;
+    [[nodiscard]] us getAverageRenderTime() const;
+    [[nodiscard]] us getAverageFrameTime() const;
+
 private:
     void startSystemTimer();    // Called in constructor
     us stopSystemTimer();     // Called inside writePerformanceData()
@@ -45,9 +54,24 @@ private:
     bool fftTimerRunning = false;
     bool renderTimerRunning = false;
 
+
+    //  Count the occurrences of each timer cycle
     unsigned long long frameCounter = 0;
     unsigned long long fftCounter = 0;
     unsigned long long renderCounter = 0;
+
+
+    //  Count the total time for all occurrences to calculate averages
+    //  Measured in Microseconds (us)
+    unsigned long long frameTimeAccumulator_us = 0;
+    unsigned long long fftTimeAccumulator_us = 0;
+    unsigned long long renderTimeAccumulator_us = 0;
+
+
+    //  Most recent data for each timer
+    unsigned long long mostRecentFrameTime_us = 0;
+    unsigned long long mostRecentFFTTime_us = 0;
+    unsigned long long mostRecentRenderTime_us = 0;
 
     //  I have yet to choose a parsing strategy. Probably binning the data in a histogram as it is parsed.
     //  void binFrameTime(us);
